92b1cdfc1b8f0569fc3b2f945ac5959327372bad
nanom80 <nanomegi@gmail.com>
2025-04-21 12:49:49 +0900

    2025-04-21:2nd

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,36 @@
+package com.javaex.practice;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		if(i == 0) {
+			
+		}
+		
+		int j = 0;
+		
+		if(j < 0) {
+			
+		}
+		
+		switch('A') {
+		
+		}
+		
+		switch(100) {
+		
+		}
+		
+		switch("A") {
+		
+		}
+		
+		String name = "";
+		
+		if(name == "홍길동") {
+			
+		}
+	}
+}
---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,7 @@
+package com.javaex.practice;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		
+	}
+}
